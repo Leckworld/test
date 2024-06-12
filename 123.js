772491eed0d6d2962eba99d0b012295b884a88b8
Leckworld <103003740+Leckworld@users.noreply.github.com>
2024-06-12 17:41:12 +0530

    123.js

--- 123.js
+++ 123.js
@@ -229,7 +229,19 @@ console.log("B" + b);
 }
 
 
-
+var firebaseConfig = {
+        apiKey: "AIzaSyAmvWtPCfVh8Mm4WkPy1SKu6BQAMTtm-kE",
+        authDomain: "home-b28fd.firebaseapp.com",
+        databaseURL: "https://home-b28fd-default-rtdb.firebaseio.com",
+        projectId: "home-b28fd",
+        storageBucket: "home-b28fd.appspot.com",
+        messagingSenderId: "487868980428",
+        appId: "1:487868980428:web:af634b774933ff66eeb669",
+        measurementId: "G-95ZTXMGX52"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 
 
