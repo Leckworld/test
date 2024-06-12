7706b5698a043fd7eb7eaa3abb4cc797d26f4306
Leckworld <103003740+Leckworld@users.noreply.github.com>
2024-06-12 17:31:45 +0530

    123.js

--- 123.js
+++ 123.js
@@ -233,6 +233,13 @@ console.log("B" + b);
 
 
 
+firebase.auth().onAuthStateChanged((user)=>{
+    if(!user){
+        location.replace("index.html")
+    }else{
+        document.getElementById("user").innerHTML = "Hello, "+user.email
+    }
+})
 
 
 
